0b52199657696691ef431db359af315351242a69
larsonc3385 <larsonc3385@my.uwstout.edu>
2025-12-02 22:41:34 -0600

    BinderBase 3.1: adjusted the files a little

--- BinderBase/src/router/index.js
+++ BinderBase/src/router/index.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import home from '../components/home.vue'
 import deckPage from '../components/deckPage.vue'
 
 const routes = [
 { path: '/', redirect: '/deckPage'},
-{ path:'/home', name: 'home', component: home},
 { path: '/deckPage', name: deckPage, component: deckPage }
 ]
 
